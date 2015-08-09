989a43f633b968caf314b001fe4a3647a092c424
Josh Carlson <magneticstain@gmail.com>
2015-08-09 16:09:40 -0400

    Created file & class structure, including names and descriptions in docstring

---
+++ BuffettBot-orig/bot.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+BuffettBot // Bot.py
+
+The main actor in the entire BuffettBot application.
+"""
+
+# MODULES
+# | Native
+
+# | Third-Party
+
+# | Custom
+
+
+# METADATA
+__author__ = 'Joshua Carlson-Purcell'
+__copyright__ = 'Copyright 2015, CarlsoNet'
+__license__ = 'MIT'
+__version__ = '1.0.0-alpha'
+__maintainer__ = 'Joshua Carlson-Purcell'
+__email__ = 'jcarlson@carlso.net'
+__status__ = 'Prototype'
+
+# FUNCTIONS
+
+# MAIN
+def main():
+    print("########################################")
+    print("BuffettBot v1.0.0-alpha")
+    print("########################################")
+
+if __name__ == '__main__':
+    main()
